39434fc687fa413a853bae971a0c041ff7445c81
Martin Seeler <mseeler@avantgarde-labs.de>
2022-04-12 16:13:04 +0200

    Add i18n to new badges

--- components/serp-badge.tsx
+++ components/serp-badge.tsx
@@ -1,24 +1,22 @@
-import {
-  HashtagIcon,
-  LocationMarkerIcon,
-  TagIcon,
-} from "@heroicons/react/outline";
+import { HashtagIcon, LocationMarkerIcon } from "@heroicons/react/outline";
+import { useTranslation } from "next-i18next";
 import React, { FC } from "react";
 
 const SerpBadge: FC<{
   value: string;
   colors: string;
+  facetId: string;
   icon: (props: React.SVGProps<SVGSVGElement>) => JSX.Element;
 }> = (props) => {
+  const { t } = useTranslation();
   return (
     <span
       className={
         "inline-flex items-center text-xs font-medium space-x-1 " + props.colors
       }
-      title=""
     >
       <props.icon className="h-4 w-4" aria-hidden="true" />
-      <p>{props.value}</p>
+      <p>{t(`facet_${props.facetId}_value_${props.value}`, props.value)}</p>
     </span>
   );
 };
@@ -29,6 +27,7 @@ export const GeoSerpBadge: FC<{ value: string }> = ({ value }) => {
       value={value}
       //   colors="bg-amber-100 text-amber-800"
       colors="text-amber-600"
+      facetId="region_country_city"
       icon={LocationMarkerIcon}
     />
   );
@@ -40,6 +39,7 @@ export const IntentSerpBadge: FC<{ value: string }> = ({ value }) => {
       value={value}
       //   colors="bg-green-100 text-green-800"
       colors="text-green-600"
+      facetId="what"
       icon={HashtagIcon}
     />
   );
